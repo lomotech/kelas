b1540bc8cb0bbc3686a51702297fac06327a10c6
Muhammad Syafiq Bin Zainuddin <tmukmkd@gmail.com>
2021-07-15 09:20:01 +0800

    Add tables.

---
+++ database/migrations/2021_07_15_011234_create_class_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('child_id')->constrained('children');
+            $table->foreignId('child_class_id')->constrained('child_classes');
+            $table->datetime('class_datetime');
+            $table->string('url')->nullable();
+            $table->text('description');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('class_schedules');
+    }
+}
